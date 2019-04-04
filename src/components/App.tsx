03f7028c265367c5e0be751db5d4fb0706b4b23a
ietomi on mac <ssbnj838@yahoo.co.jp>
2019-04-04 21:57:13 +0900

    debug source add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,8 +10,23 @@ import { addTodo } from "../actions";
 
 import MyComponent from "./MyReduxSample";
 
+
+
+// redux のデバッガーを使う場合は以下をコメントアウト.
+// declare global {
+//     interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+// }
+
+// const store = createStore(
+//     rootReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__
+//         && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
 // createStore の時点で rootReducer に登録した関数が 1 回動く.
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer
+);
 
 // store に addTodo('Hello World!') で表される action が dispatch されたときに、
 // rootReducer に登録した関数が動き、todo 関数の返り値が store の state となる。
